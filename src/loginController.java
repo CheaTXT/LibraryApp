b3cabd484d19399fc90fa6bf1d2047f9010158e2
CheaTXT <serey.vichea9999@gmail.com>
2023-06-07 17:28:51 +0700

    update

---
+++ src/loginController.java
@@ -0,0 +1,50 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import java.io.IOException;
+
+public class loginController {
+  private Stage stage;
+  private Scene scene;
+
+  @FXML
+  public void switchtohome(ActionEvent event) throws IOException {
+    Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
+    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+}
+
+// public class homecontroller {
+// private Stage stage;
+// private Scene scene;
+// private Parent root;
+// public void switchtoaboutus(ActionEvent event) throws IOException {
+// Parent root= FXMLLoader.load(getClass().getResource("AboutUS.fxml"));
+// stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+// scene = new Scene(root);
+// stage.setScene(scene);
+// stage.show();
+// }
+// public void switchtocategories(ActionEvent event)throws IOException {
+// Parent root= FXMLLoader.load(getClass().getResource("Categories.fxml"));
+// stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+// scene = new Scene(root);
+// stage.setScene(scene);
+// stage.show();
+// }
+// public void switchtologin(ActionEvent event) throws IOException {
+// Parent root= FXMLLoader.load(getClass().getResource("user_login.fxml"));
+// stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+// scene = new Scene(root);
+// stage.setScene(scene);
+// stage.show();
+// }
+
+// }
